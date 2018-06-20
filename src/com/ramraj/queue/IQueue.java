2ef94fe572056e2fd0f0a44aae843f90c1afede7
Ramraj S <sramraj2@gmail.com>
2018-06-20 15:48:51 +0530

    Initialize a queue interface with basic methods

---
+++ src/com/ramraj/queue/IQueue.java
@@ -0,0 +1,62 @@
+package com.ramraj.queue;
+
+import com.ramraj.queue.exception.EmptyQueueException;
+/**
+ * Generic interface to define the features of my queues
+ * 
+ * @author Ramraj
+ *
+ * @param <T> - Defines the type of object that this queue 
+ * 				is going to handle
+ */
+public interface IQueue<T> {
+	/**
+	 * Returns the item on the head of the queue and
+	 * removes it from the queue
+	 * 
+	 * @return item on the head
+	 * @throws EmptyQueueException if queue is empty 
+	 */
+	public T get() throws EmptyQueueException;
+	
+	/**
+	 * Add the item to the queue's tail
+	 * 
+	 * @param item
+	 * @return
+	 */
+	public boolean put(T item);
+	
+	/**
+	 * Returns the items on the head but doesn't remove it
+	 * from the queue
+	 * 
+	 * @return
+	 */
+	public boolean peek();
+	
+	/**
+	 * @return true  if the queue is empty
+	 * 		   false if the queue is not empty
+	 */
+	public boolean isEmpty();
+	
+	/**
+	 * @return true  if the queue is full
+	 * 		   false if the queue is not full
+	 */
+	public boolean isFull();
+	
+	/**
+	 * Prints the items in the queue
+	 */
+	public void print();
+	
+	/**
+	 * Resizes the array with given value
+	 * 
+	 * @param size New size for the array
+	 * @return
+	 */
+	public boolean resize(int size);
+}
